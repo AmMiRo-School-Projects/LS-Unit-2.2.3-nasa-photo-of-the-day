9e1c5db0ca5eb9b47a07ad827ed389d49a655e23
Amos Rose <amosroseacct@gmail.com>
2020-01-22 14:59:42 -0700

    api working and initial file layout done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Header from "./components/Header/HeaderCard";
 import "./App.css";
 
 function App() {
+  const [nasaData, setNasaData] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=Tn29fbPFU60KjrekRWjh1encJfdOiKXfPb3Q0KQX"
+      )
+      .then(response => {
+        console.log(response);
+        setNasaData(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/HeaderCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="App-header">
+      <img src="./assest/nasa-logo.png" alt="NASA logo" />
+      <h1>Astronomy Picture of the Day</h1>
+    </div>
+  );
+}
+
+export default Header;
